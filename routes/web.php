72e39f12b3440258ac806e49a6ff2caee8f1a448
zaidatulnaima@gmail.com <zaidatulnaima@gmail.com>
2024-05-09 00:56:51 +0700

    template 1/2 update

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
+Route::get('/index', function () {
     return view('index');
+});
+
+Route::get('/upload', function () {
+    return view('upload');
+});
+
+Route::get('/prediksi', function () {
+    return view('prediksi');
 });
